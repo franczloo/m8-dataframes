652293d3c68c1b5b27c2b8c1c7e64530ce9d11b5
franczloo <franczloo@gmail.com>
2016-10-13 15:45:31 -0700

    readding deleted files

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -1,37 +1,44 @@
-# Exercise 4: Working with Data Frames
+# Load R's "USPersonalExpenditure" dataest using the "data()" function
+data("USPersonalExpenditure")
 
-# Load R's "USPersonalExpenditure" dataest using the `data()` function
-
-
-# The variable USPersonalExpenditure is now accessible to you. Unfortunately, it's not a data.frame
+# The variable USPersonalExpenditure is now accessible to you.  Unfortunately, it's not a data.frame
 # Test this using the is.data.frame function
-
+is.data.frame(USPersonalExpenditure)
 
 # Luckily, you can simply pass the USPersonalExpenditure variable to the data.frame function
 # to convert it a data.farme
 
 # Create a new variable by passing the USPersonalExpenditure to the data.frame function
-
+usp <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
-
+colnames(usp)
 
 # Why are they so strange?
 
-
 # What are the row names of your dataframe?
+rownames(usp)
 
-
-# Create a column `category` that is equal to your rownames
-
+# Create a column "category" that is equal to your rownames
+usp$category <- rownames(usp)
 
 # How much money was spent on personal care in 1940?
-
+care.1940 <- usp['Personal Care', 'X1940']
 
 # How much money was spent on Food and Tobacco in 1960
-
+food.1960 <- usp['Food and Tobacco', 'X1960']
 
 # What was the highest expenditure category in 1960?
+highest.1960 <- usp$category[usp$X1960 == max(usp$X1960)]
+
+# Write a function that takes in a year as a parameter, and
+# returns the highest spending category of that year
+highest <- function(year) {
+  return(usp$category[usp[year] == max(usp[year])])
+}
+
+# Using your function, determine the highest spending category of each year
+highest.1950 <- highest('X1950')
 
 
 ### Bonus ###
